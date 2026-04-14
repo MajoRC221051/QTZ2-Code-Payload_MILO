d2fc9597fd1174c055aa99f375c9f4ed43e2a7ac
MajoRC221051 <110446328+MajoRC221051@users.noreply.github.com>
2026-04-14 11:15:42 -0600

    Update Manual_Capture_openmv.py

--- I2C_Protocol/Manual_Capture_openmv.py
+++ I2C_Protocol/Manual_Capture_openmv.py
@@ -1,18 +1,36 @@
-# Test 1 - I2C communication and physical test - Feb 20 2026
+# Capture with I2C and automatic reset Feb 25 2026
 
+import sensor
 from pyb import I2C
-from pyb import LED
+import machine
+
+sensor.reset()  # Reset and initialize the sensor.
+sensor.set_pixformat(sensor.RGB565)  # Set pixel format to RGB565 (or GRAYSCALE)
+sensor.set_framesize(sensor.QVGA)  # Set frame size to QVGA (320x240)
+sensor.skip_frames(time=2000)  # Wait for settings take effect.
 
 i2c = I2C(2, I2C.SLAVE, addr=0x12)
-led = LED(1)
+
+capture_count = 0 
+
+led = machine.LED("LED_BLUE")
 
 while True:
+    led.toggle()
+    sensor.snapshot()
     try:
-        data = i2c.recv(1)  # Espera 1 byte
+        data = i2c.recv(1, timeout=500) # Delay
 
-        if data == b'\x10':
-            led.on()
+        print("Dato recibido:", data)
+
+        if data == b'\x55':
+            capture_count+=1
+            img = sensor.snapshot()
+            print ("Captura #", capture_count)
+            print ("Captura lista")
+            led.off()
+            img.save("example.jpg")
+            #machine.reset()
 
     except OSError:
-        # No hay datos disponibles
         pass
