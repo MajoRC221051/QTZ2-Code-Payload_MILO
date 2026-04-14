b7fc3c07c66ff75f99ae2909a531cd5633bca64c
MajoRC221051 <110446328+MajoRC221051@users.noreply.github.com>
2026-04-14 11:14:36 -0600

    Replace UART with I2C for command handling
    Refactor code to use I2C communication instead of UART for commands. Implement LED control based on received I2C data.

--- I2C_Protocol/Manual_Capture_openmv.py
+++ I2C_Protocol/Manual_Capture_openmv.py
@@ -1,48 +1,18 @@
-import sensor
-import image
-import time
-import os
-from pyb import UART
-from machine import reset
+# Test 1 - I2C communication and physical test - Feb 20 2026
 
-sensor.reset()
-sensor.set_pixformat(sensor.RGB565)
-sensor.set_framesize(sensor.QVGA)
-sensor.skip_frames(time=2000)
+from pyb import I2C
+from pyb import LED
 
-# --------- UART ---------
-uart = UART(3, 115200, timeout_char=1000)
+i2c = I2C(2, I2C.SLAVE, addr=0x12)
+led = LED(1)
 
-photo_counter = 0
+while True:
+    try:
+        data = i2c.recv(1)  # Espera 1 byte
 
-print("System ready, waiting for command")
+        if data == b'\x10':
+            led.on()
 
-
-while(True):
-
-    if uart.any():
-        cmd = uart.readline()
-
-        if cmd is not None:
-            cmd = cmd.decode().strip()
-
-            if cmd == "TAKE":
-                img = sensor.snapshot()
-                filename = "/photo_%d.jpg" % photo_counter
-                img.save(filename)
-                uart.write("OK\n")
-                photo_counter += 1
-
-            elif cmd == "DELETE":
-                for file in os.listdir():
-                    if file.endswith(".jpg"):
-                        os.remove(file)
-                uart.write("DELETED\n")
-
-            elif cmd == "RESET":
-                uart.write("RESETTING\n")
-                time.sleep_ms(100)
-                reset()
-
-            else:
-                uart.write("UNKNOWN\n")
+    except OSError:
+        # No hay datos disponibles
+        pass
