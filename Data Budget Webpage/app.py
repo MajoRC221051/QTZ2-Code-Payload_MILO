e415774e8177944548e98453ef20bf3d64fe878b
MajoRC221051 <110446328+MajoRC221051@users.noreply.github.com>
2026-06-30 21:48:07 -0600

    Fix page switch path for OpenMV Cam RT1062

--- Data Budget Webpage/app.py
+++ Data Budget Webpage/app.py
@@ -54,7 +54,7 @@ col1, col2 = st.columns(2)
 with col1:
     if st.button("OpenMV Cam RT1062"):
         st.session_state["cam_selected"] = "OpenMV Cam RT1062"
-        st.switch_page("pages/calculator.py")
+        st.switch_page("calculator.py")
 with col2:
     if st.button("OpenMV Cam H7 Plus"):
         st.session_state["cam_selected"] = "OpenMV Cam H7 Plus"
