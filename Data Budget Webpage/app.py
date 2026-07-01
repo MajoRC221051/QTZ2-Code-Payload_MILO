d27ad534743eea075785eb05229ec986265d9c50
MajoRC221051 <110446328+MajoRC221051@users.noreply.github.com>
2026-06-30 21:46:57 -0600

    Fix case sensitivity in page switch for calculator

--- Data Budget Webpage/app.py
+++ Data Budget Webpage/app.py
@@ -54,11 +54,11 @@ col1, col2 = st.columns(2)
 with col1:
     if st.button("OpenMV Cam RT1062"):
         st.session_state["cam_selected"] = "OpenMV Cam RT1062"
-        st.switch_page("Pages/calculator.py")
+        st.switch_page("pages/calculator.py")
 with col2:
     if st.button("OpenMV Cam H7 Plus"):
         st.session_state["cam_selected"] = "OpenMV Cam H7 Plus"
-        st.switch_page("Pages/calculator.py")
+        st.switch_page("pages/calculator.py")
 
 
 
